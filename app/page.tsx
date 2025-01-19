c69d73662873c5e78409be3e66c3e674abc7516c
Jess (Yan Tung) Lam <jesslam321@gmail.com>
2025-01-19 18:56:42 +0000

    fix background colour

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import CursorDot from '@/components/Cursor';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
+    <main className="min-h-screen flex items-center justify-center bg-white">
       <div className="w-full px-4">
         <CursorDot />
         <Header/>
